edc654611df52e84a9b1bc0c4088df48f5e8bd3d
Raj kumar <rajuttam4447@gmail.com>
2022-08-04 22:33:45 +0530

    Autoplay carousel, added texture, Owner info, marquee tag added

---
+++ main.js
@@ -0,0 +1,34 @@
+
+
+
+
+    
+
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        margin:10,
+        nav:false,
+        dots: false,
+        autoplay:true,
+        autoplayTimeout:2000,
+        autoplayHoverPause:true,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:1
+            },
+            1000:{
+                items:2
+            },
+            1024:{
+                item: 2
+            }
+        }
+    })
+
+
+
+
+
